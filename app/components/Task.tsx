90434c44822c5771f497689befa8b5932e22291f
Jacob Stoebel <jstoebel@gmail.com>
2018-09-17 13:10:14 -0400

    use extracted task interface

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
+import TaskI from '../interfaces/Task';
 
-interface TaskProps {
+interface TaskProps extends TaskI {
   key: number,
-  id: number,
-  name: string,
-  description: string,
 }
 
 const Task: React.SFC<TaskProps> = (props) => {
